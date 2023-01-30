664a1c414a3e04595c2a894a95cf7471b02bf110
Icaro Gabriel <100494049+Icaroiz@users.noreply.github.com>
2023-01-30 15:31:12 -0300

    Order, Instant, ISO 8601 part 2

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,28 +1,40 @@
 package com.educandoweb.course.config;
 
- import java.util.Arrays;
+import java.time.Instant;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
- @Configuration
- @Profile("test")
- public class TestConfig implements CommandLineRunner {
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
 
- 	@Autowired
- 	private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
- 	@Override
- 	public void run(String... args) throws Exception {
+	@Autowired
+	private OrderRepository orderRepository;
 
- 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
- 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+	@Override
+	public void run(String... args) throws Exception {
 
- 		userRepository.saveAll(Arrays.asList(u1, u2));
- 	}
- }
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+	}
+}
--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,20 +16,22 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
+public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	public Order() {
-		
+
 	}
 
 	public Order(Long id, Instant moment, User client) {
@@ -77,5 +81,5 @@ public class Order implements Serializable{
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
